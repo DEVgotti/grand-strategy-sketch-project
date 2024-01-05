91c324c854203e694bad7e6f02696577d09e1144
Gotti <david.lmonras@gmail.com>
2024-01-05 12:53:34 +0100

    files setup

---
+++ main.js
@@ -0,0 +1,8 @@
+const strategy = () => {
+  const app = document.getElementById("app")
+  const container = app.appendChild(document.createElement('div'))
+  container.classList.add('cell')
+
+}
+
+strategy()
